c1bb26cf11998364b15ee4bd9fd90d48a56cdbea
aditgocendra <gocendra123@gmail.com>
2022-11-16 11:04:48 +0700

    using tabbarview for content page

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,23 +1,14 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class HomeController extends GetxController {
-  //TODO: Implement HomeController
+class HomeController extends GetxController
+    with GetSingleTickerProviderStateMixin {
+  late TabController tabController;
 
-  final count = 0.obs;
   @override
   void onInit() {
     super.onInit();
-  }
 
-  @override
-  void onReady() {
-    super.onReady();
+    tabController = TabController(length: 5, vsync: this);
   }
-
-  @override
-  void onClose() {
-    super.onClose();
-  }
-
-  void increment() => count.value++;
 }
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,202 +1,110 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
 import '../../../core/constant/color_constant.dart';
 import '../controllers/home_controller.dart';
+import 'content/home..dart';
 
 class HomeView extends GetView<HomeController> {
   const HomeView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: backgroundColor,
+        shadowColor: Colors.transparent,
+        flexibleSpace: AppBarCustom(),
+        toolbarHeight: 100,
+      ),
       body: Container(
-        margin: const EdgeInsets.only(
+        padding: const EdgeInsets.only(
           left: 56,
-          top: 24,
-          bottom: 16,
           right: 18,
         ),
-        child: Stack(
-          children: [
-            // Appbar
-            const AppBarCustom(),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                // Content
-                Row(
-                  children: const [
-                    Expanded(
-                      child: LeftContent(),
-                    ),
-                    Expanded(
-                      child: RighContent(),
-                    )
-                  ],
+        child: TabBarView(
+          controller: controller.tabController,
+          children: const [
+            Home(),
+            Center(
+              child: Text("2"),
+            ),
+            Center(
+              child: Text("3"),
+            ),
+            Center(
+              child: Text("4"),
+            ),
+            Center(
+              child: Text("5"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class AppBarCustom extends StatelessWidget {
+  AppBarCustom({super.key});
+
+  final controller = Get.find<HomeController>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(
+        left: 56,
+        top: 20,
+        right: 18,
+      ),
+      child: Row(
+        children: [
+          const Text(
+            'Aditya Gocendra',
+            style: TextStyle(
+              fontSize: 32,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const Spacer(),
+          SizedBox(
+            width: 650,
+            child: TabBar(
+              indicator: const ShapeDecoration(
+                shape: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: primaryColor,
+                    width: 2,
+                    style: BorderStyle.solid,
+                  ),
+                ),
+              ),
+              padding: EdgeInsets.zero,
+              indicatorColor: Colors.black,
+              labelColor: primaryColor,
+              unselectedLabelColor: Colors.white,
+              controller: controller.tabController,
+              tabs: const [
+                Tab(
+                  text: "Home",
+                ),
+                Tab(
+                  text: "Recent Work",
+                ),
+                Tab(
+                  text: "About",
+                ),
+                Tab(
+                  text: "Education",
+                ),
+                Tab(
+                  text: "Contact",
                 ),
               ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
-  }
-}
-
-class RighContent extends StatelessWidget {
-  const RighContent({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Image.asset(
-        'assets/images/image_profile.png',
-        width: 320,
-      ),
-    );
-  }
-}
-
-class LeftContent extends StatelessWidget {
-  const LeftContent({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        const Text(
-          "Hi There,",
-          style: TextStyle(
-            fontSize: 72,
-            fontWeight: FontWeight.bold,
-            height: 1,
-          ),
-        ),
-        Row(
-          children: const [
-            Text(
-              "I'm ",
-              style: TextStyle(
-                fontSize: 52,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Text(
-              "Aditya Gocendra",
-              style: TextStyle(
-                fontSize: 52,
-                fontWeight: FontWeight.bold,
-                color: primaryColor,
-              ),
-            ),
-          ],
-        ),
-        const SizedBox(
-          height: 16,
-        ),
-        Row(
-          children: [
-            ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                backgroundColor: primaryColor,
-                shape: const StadiumBorder(),
-                minimumSize: const Size(210, 50),
-              ),
-              child: const Text("Portfolio"),
-            ),
-            const SizedBox(
-              width: 24,
-            ),
-            OutlinedButton(
-              onPressed: () {},
-              style: OutlinedButton.styleFrom(
-                side: const BorderSide(
-                  color: primaryColor,
-                ),
-                shape: const StadiumBorder(),
-                minimumSize: const Size(210, 50),
-              ),
-              child: const Text(
-                'Contact',
-                style: TextStyle(color: primaryColor),
-              ),
-            ),
-          ],
-        ),
-      ],
-    );
-  }
-}
-
-class AppBarCustom extends StatefulWidget {
-  const AppBarCustom({super.key});
-
-  @override
-  State<AppBarCustom> createState() => _AppBarCustomState();
-}
-
-class _AppBarCustomState extends State<AppBarCustom>
-    with TickerProviderStateMixin {
-  late TabController tabController;
-
-  @override
-  void initState() {
-    super.initState();
-    tabController = TabController(length: 4, vsync: this);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        const Text(
-          'Aditya Gocendra',
-          style: TextStyle(
-            fontSize: 32,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        const Spacer(),
-        Expanded(
-          child: TabBar(
-            indicator: const ShapeDecoration(
-              shape: UnderlineInputBorder(
-                borderSide: BorderSide(
-                  color: primaryColor,
-                  width: 2,
-                  style: BorderStyle.solid,
-                ),
-              ),
-            ),
-            padding: EdgeInsets.zero,
-            indicatorColor: Colors.black,
-            labelColor: primaryColor,
-            unselectedLabelColor: Colors.white,
-            controller: tabController,
-            tabs: const [
-              Tab(
-                text: "Recent Work",
-              ),
-              Tab(
-                text: "About",
-              ),
-              Tab(
-                text: "Education",
-              ),
-              Tab(
-                text: "Contact",
-              ),
-            ],
-          ),
-        ),
-      ],
-    );
+    ;
   }
 }
