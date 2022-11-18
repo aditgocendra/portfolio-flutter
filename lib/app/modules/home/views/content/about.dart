2048b887a63f9dd84c6463854dd83fd59a4e8eab
aditgocendra <gocendra123@gmail.com>
2022-11-18 21:45:27 +0700

    checkout project redirect

--- lib/app/modules/home/views/content/about.dart
+++ lib/app/modules/home/views/content/about.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
+import 'package:portfolio/app/modules/home/controllers/home_controller.dart';
 import '../../../../core/constant/color_constant.dart';
 import '../../../../core/constant/values.dart';
 
@@ -8,6 +10,8 @@ class About extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final controller = Get.find<HomeController>();
+
     return Column(
       mainAxisAlignment: MainAxisAlignment.spaceAround,
       children: [
@@ -22,51 +26,60 @@ class About extends StatelessWidget {
           padding: const EdgeInsets.only(right: 32),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: listAboutMenu.map(
-              (val) {
-                return Container(
-                  margin: const EdgeInsets.symmetric(horizontal: 32),
-                  width: 430,
-                  height: 481,
-                  decoration: BoxDecoration(
-                    color: secondaryPrimaryColor,
-                    borderRadius: BorderRadius.circular(64),
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Container(
-                        margin: const EdgeInsets.only(top: 48),
-                        child: SvgPicture.asset(
-                          val['assets'],
-                          color: primaryColor,
-                        ),
+            children: listAboutMenu
+                .asMap()
+                .map(
+                  (index, val) => MapEntry(
+                    index,
+                    Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 32),
+                      width: 430,
+                      height: 481,
+                      decoration: BoxDecoration(
+                        color: secondaryPrimaryColor,
+                        borderRadius: BorderRadius.circular(64),
                       ),
-                      Container(
-                        margin: const EdgeInsets.all(32),
-                        child: ElevatedButton(
-                          onPressed: () {},
-                          style: ElevatedButton.styleFrom(
-                            shape: const StadiumBorder(),
-                            minimumSize: const Size.fromHeight(70),
-                            shadowColor: Colors.transparent,
-                            backgroundColor: secondaryLightColor,
-                          ),
-                          child: const Text(
-                            "Checkout",
-                            style: TextStyle(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Container(
+                            margin: const EdgeInsets.only(top: 48),
+                            child: SvgPicture.asset(
+                              val['assets'],
                               color: primaryColor,
-                              fontSize: 20,
                             ),
                           ),
-                        ),
-                      )
-                    ],
+                          Container(
+                            margin: const EdgeInsets.all(32),
+                            child: ElevatedButton(
+                              onPressed: () {
+                                controller.tabNavBarController.index = 3;
+                                controller.tabRecentWorkController.index =
+                                    index;
+                              },
+                              style: ElevatedButton.styleFrom(
+                                shape: const StadiumBorder(),
+                                minimumSize: const Size.fromHeight(70),
+                                shadowColor: Colors.transparent,
+                                backgroundColor: secondaryLightColor,
+                              ),
+                              child: const Text(
+                                "Checkout",
+                                style: TextStyle(
+                                  color: primaryColor,
+                                  fontSize: 20,
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
                   ),
-                );
-              },
-            ).toList(),
+                )
+                .values
+                .toList(),
           ),
         )
       ],
