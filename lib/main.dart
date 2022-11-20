68e2a7d918a1ea720677ec2d0dc04e785c22cfb0
aditgocendra <gocendra123@gmail.com>
2022-11-20 13:48:02 +0700

    load env

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'app/core/constant/color_constant.dart';
 
 import 'app/routes/app_pages.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: ".env");
   runApp(const App());
 }
 
