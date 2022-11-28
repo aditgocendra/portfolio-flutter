8430f4d6d1c7e1d30e946dba766f58726dcaccba
aditgocendra <gocendra123@gmail.com>
2022-11-28 11:04:11 +0700

    update responsve screen

--- lib/app/modules/home/views/content/about.dart
+++ lib/app/modules/home/views/content/about.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
-import 'package:portfolio/app/modules/home/controllers/about_controller.dart';
-import 'package:portfolio/app/modules/home/controllers/portfolio_controller.dart';
+import '../../../../core/utility/screen_size_util.dart';
 import '../../../../core/constant/color_constant.dart';
 import '../../../../core/constant/values.dart';
+import '../../controllers/about_controller.dart';
+import '../../controllers/portfolio_controller.dart';
 import '../../controllers/home_controller.dart';
 
 class About extends GetView<AboutController> {
@@ -18,15 +19,15 @@ class About extends GetView<AboutController> {
     int crossAxis = 3;
     double heightIcon = sizeScreenWidth / 10.5;
 
-    if (sizeScreenWidth < 650) {
+    if (UtilityScreenSize().isSmall(context)) {
       crossAxis = 1;
-      heightIcon = sizeScreenWidth / 4.5;
-    } else if (sizeScreenWidth < 1000) {
+      heightIcon = sizeScreenWidth / 5.8;
+    } else if (UtilityScreenSize().isMedium(context)) {
       crossAxis = 2;
-      heightIcon = sizeScreenWidth / 8.5;
+      heightIcon = sizeScreenWidth / 8.8;
     } else {
       crossAxis = 3;
-      heightIcon = sizeScreenWidth / 10.5;
+      heightIcon = sizeScreenWidth / 9;
     }
 
     return ListView(
@@ -41,68 +42,93 @@ class About extends GetView<AboutController> {
         const SizedBox(
           height: 100,
         ),
-        GridView.builder(
-          padding: EdgeInsets.only(
-            right: sizeScreenWidth > 1000 ? 32 : 0,
+        if (!UtilityScreenSize().isVerySmall(context))
+          GridView.builder(
+            padding: EdgeInsets.only(
+              right: UtilityScreenSize().isSmall(context) ? 0 : 32,
+            ),
+            shrinkWrap: true,
+            primary: false,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: crossAxis,
+              childAspectRatio: 1.1,
+              mainAxisSpacing: 24,
+            ),
+            itemCount: listAboutMenu.length,
+            itemBuilder: (context, index) {
+              return CardProject(
+                heightIcon: heightIcon,
+                index: index,
+                homeController: homeController,
+                portfolioController: portfolioController,
+              );
+            },
           ),
-          shrinkWrap: true,
-          primary: false,
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: crossAxis,
-            childAspectRatio: 1.1,
-            mainAxisSpacing: 24,
-          ),
-          itemCount: listAboutMenu.length,
-          itemBuilder: (context, index) {
-            return Container(
-              margin: const EdgeInsets.symmetric(horizontal: 32),
-              decoration: BoxDecoration(
-                color: secondaryPrimaryColor,
-                borderRadius: BorderRadius.circular(64),
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(
-                    margin: const EdgeInsets.only(top: 48),
-                    child: SvgPicture.asset(
-                      listAboutMenu[index]['assets'],
-                      color: primaryColor,
-                      height: heightIcon,
-                    ),
-                  ),
-                  Container(
-                    margin: const EdgeInsets.all(32),
-                    child: ElevatedButton(
-                      onPressed: () {
-                        homeController.tabNavBarController.index = 3;
-                        portfolioController.tabPortfolioController.index =
-                            index;
-                      },
-                      style: ElevatedButton.styleFrom(
-                        shape: const StadiumBorder(),
-                        minimumSize: Size.fromHeight(
-                          sizeScreenWidth > 650 ? 70 : 55,
-                        ),
-                        shadowColor: Colors.transparent,
-                        backgroundColor: secondaryLightColor,
-                      ),
-                      child: const Text(
-                        "Checkout",
-                        style: TextStyle(
-                          color: primaryColor,
-                          fontSize: 20,
-                        ),
-                      ),
-                    ),
-                  )
-                ],
-              ),
-            );
-          },
-        ),
       ],
+    );
+  }
+}
+
+class CardProject extends StatelessWidget {
+  final double heightIcon;
+  final int index;
+  final HomeController homeController;
+  final PortfolioController portfolioController;
+
+  const CardProject({
+    Key? key,
+    required this.heightIcon,
+    required this.index,
+    required this.homeController,
+    required this.portfolioController,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 32),
+      decoration: BoxDecoration(
+        color: secondaryPrimaryColor,
+        borderRadius: BorderRadius.circular(64),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            margin: const EdgeInsets.only(top: 48),
+            child: SvgPicture.asset(
+              listAboutMenu[index]['assets'],
+              color: primaryColor,
+              height: heightIcon,
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.all(32),
+            child: ElevatedButton(
+              onPressed: () {
+                homeController.tabNavBarController.index = 3;
+                portfolioController.tabPortfolioController.index = index;
+              },
+              style: ElevatedButton.styleFrom(
+                shape: const StadiumBorder(),
+                minimumSize: Size.fromHeight(
+                  UtilityScreenSize().isSmall(context) ? 50 : 70,
+                ),
+                shadowColor: Colors.transparent,
+                backgroundColor: secondaryLightColor,
+              ),
+              child: Text(
+                "Checkout",
+                style: TextStyle(
+                  color: primaryColor,
+                  fontSize: UtilityScreenSize().isSmall(context) ? 16 : 20,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
