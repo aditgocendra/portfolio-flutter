d45a2660de745433e81fe0d8668d4083a3f5f3f9
aditgocendra <gocendra123@gmail.com>
2022-11-29 13:36:46 +0700

    add firebase core and hosting

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:portfolio/app/core/binding/initial_binding.dart';
+import 'package:portfolio/firebase_options.dart';
 import 'app/core/constant/color_constant.dart';
 
 import 'app/routes/app_pages.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await dotenv.load(fileName: ".env");
   runApp(const App());
 }
