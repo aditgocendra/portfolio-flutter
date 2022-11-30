c4cad77a59d0f63417cd0186aef8b60d22c0c6f4
aditgocendra <gocendra123@gmail.com>
2022-12-01 00:03:33 +0700

    update

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -2,11 +2,64 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:portfolio/app/core/constant/values.dart';
 
+import '../../../data/repository/email_sender_repository.dart';
+
 class HomeController extends GetxController
     with GetSingleTickerProviderStateMixin {
   late TabController tabNavBarController;
 
   final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+
+  final TextEditingController textNameEdt = TextEditingController();
+  final TextEditingController textEmailEdt = TextEditingController();
+  final TextEditingController textSubjectEdt = TextEditingController();
+  final TextEditingController textMessageEdt = TextEditingController();
+
+  // Repository
+  final repoEmailSender = Get.find<EmailSenderRepository>();
+
+  Future sendEmail() async {
+    String name = textNameEdt.text.trim();
+    String email = textEmailEdt.text.trim();
+    String subject = textSubjectEdt.text.trim();
+    String message = textMessageEdt.text.trim();
+
+    if (name.isEmpty) {
+      return;
+    }
+
+    if (email.isEmpty) {
+      return;
+    }
+
+    if (subject.isEmpty) {
+      return;
+    }
+
+    if (message.isEmpty) {
+      return;
+    }
+
+    if (!GetUtils.isEmail(email)) {
+      return;
+    }
+
+    final result = await repoEmailSender.sendEmail(
+      name,
+      email,
+      subject,
+      message,
+    );
+
+    if (result.isLeft) {
+      return;
+    }
+
+    textNameEdt.clear();
+    textEmailEdt.clear();
+    textSubjectEdt.clear();
+    textMessageEdt.clear();
+  }
 
   @override
   void onInit() {
